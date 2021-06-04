a101ff44c116f71b440da82f2f8988eb2f9baf82
Cameron <EllisCHarlan@gmail.com>
2021-06-03 23:45:05 -0400

    basic implementations for filter AND, OR, NOT made TODO for better implementations for complex classes

--- Encodings/LDAP/Filter.py
+++ Encodings/LDAP/Filter.py
@@ -2,7 +2,8 @@ from enum import IntFlag
 from typing import Dict
 
 from Encodings.ASN1 import Context
-from Encodings.ASN1.EncodingClasses import Sequence, OctetString
+from Encodings.ASN1.EncodingClasses import Sequence, OctetString, Set, ContextSpecificFormatter, decode_bytes
+from .Classes import AttributeDescription, AssertionValue
 from .Enums import FilterChoice
 
 
@@ -29,6 +30,27 @@ class Filter(Context):
         return repr(self.data)
 
 
+class AND(BaseChoice):
+    tag: FilterChoice = FilterChoice.AND
+
+    def __init__(self, data: bytes):
+        self.choice = Set(data)
+        for index, filter in enumerate(self.choice.data):
+            self.choice.data[index] = filter.apply(Filter)
+
+
+class OR(AND):
+    tag: FilterChoice = FilterChoice.OR
+
+
+class Not(BaseChoice):
+    tag: FilterChoice = FilterChoice.NOT
+
+    def __init__(self, data):
+        id, _, data, _ = decode_bytes(data)
+        self.choice = ContextSpecificFormatter(id, data).apply(Filter)
+
+
 class EqualityMatch(BaseChoice):
     tag: FilterChoice = FilterChoice.Equality_Match
 
@@ -43,13 +65,28 @@ class EqualityMatch(BaseChoice):
 class Substrings(BaseChoice):
     tag = FilterChoice = FilterChoice.Substrings
 
+    def __init__(self, data: bytes):
+        self.choice = Sequence(data)
+        self.type = AttributeDescription.subclass(self.choice.data[0])
+        self.substrings = self.choice.data[1]
+        for index, substring in self.substrings.data:
+            self.substrings.data[index] = AssertionValue.subclass(self.substrings.data[index])
+        # TODO make it so that initial(limit=1), any(limit=MAX), and final(limit=1) happens. Seperate as needed
+        # in reference to above: https://datatracker.ietf.org/doc/html/rfc4511#page-21
+
 
 class GreaterOrEqual(EqualityMatch):
     tag: FilterChoice = FilterChoice.Greater_Or_Equal
 
+    def __repr__(self):
+        return f'{self.tag.name}({self.desc}>={self.val})'
+
 
 class LessOrEqual(EqualityMatch):
     tag: FilterChoice = FilterChoice.Less_Or_Equal
+
+    def __repr__(self):
+        return f'{self.tag.name}({self.desc}<={self.val})'
 
 
 class Present(BaseChoice):
@@ -62,6 +99,10 @@ class Present(BaseChoice):
 class ApproxMatch(EqualityMatch):
     tag: FilterChoice = FilterChoice.Approx_Match
 
+    def __repr__(self):
+        return f'{self.tag.name}({self.desc}~{self.val})'
+
 
 class ExtensibleMatch(BaseChoice):
     tag: FilterChoice = FilterChoice.Extensible_Match
+    # TODO implement from https://datatracker.ietf.org/doc/html/rfc4511#page-21
