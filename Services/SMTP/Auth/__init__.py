f3535674a0501c85c86760e436d7a4f3bafdb285
Cameron <EllisCHarlan@gmail.com>
2021-09-06 03:14:58 -0400

    Started AUTH extension for SMTP Implemented PLAIN authentication option

---
+++ Services/SMTP/Auth/__init__.py
@@ -0,0 +1,46 @@
+class Auth(object):
+    def __init__(self, **kwargs):
+        self.auth_methods = list(
+            filter(
+                lambda key: f'auth_{key}' in self.__class__.__dict__ and kwargs[key],
+                kwargs.keys()
+            )
+        )
+
+    def __str__(self):
+        return f"AUTH {' '.join(map(str.upper, self.auth_methods))}"
+
+    def __getitem__(self, item):
+        if item in self.auth_methods:
+            return getattr(self, f'auth_{item}')
+        else:
+            raise KeyError(f'Auth method {item} does not exist.')
+
+    def __call__(self, line):
+        method, data = line.split(' ', -1)
+        print(self[method.lower()](data))
+
+    def auth_plain(self, data):
+        # https://www.rfc-editor.org/rfc/rfc4616.html
+
+        # https://www.rfc-editor.org/rfc/rfc4422#page-17
+        # Typically, mechanisms that have special characters
+        # require these special characters to be escaped or encoded in the
+        # character string (after encoding it in a particular Unicode
+        # transformation format) using a data encoding scheme such as Base64
+
+        from base64 import b64decode  # Only import if this function gets used at least once
+        auth_data = b64decode(data).decode().split('\x00')
+
+        # check if any values of auth_data are above limit of 255 characters per RFC
+        for val in auth_data:
+            if len(val) > 255:
+                raise ValueError(f'Value size too large.\nMust be less than 255 characters.\nSize is {len(val)}')
+
+        return auth_data
+
+
+if __name__ == '__main__':
+    print(Auth().auth_methods)  # Empty list
+    print(Auth(plain=True).auth_methods)  # Only plain auth capable
+    print(Auth(plain=True))
